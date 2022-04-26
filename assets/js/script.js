6d3f4b425d755bdaac42b0751d4419e5332e594d
KyrianU <kyrian.uwaegbulem@gmail.com>
2022-04-26 19:02:44 +0000

    Add multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,6 +31,8 @@ function runGame(gameType) {
 
    if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
+    } else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -73,7 +75,9 @@ function calculateCorrectAnswer() {
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
         incrementScore();
-    } else {
+    } else if (operator === "x") {
+        return [operand1 * operand2, "multiply"];
+    }  else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
         incrementWrongAnswer();
@@ -116,5 +120,10 @@ function displaySubstractQuestion() {
 }
 
 function displayMultiplyQuestion() {
+
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 
 }
